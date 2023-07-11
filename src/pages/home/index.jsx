6257953412270021c6125f7ea2fa696b0888c7e5
dani-abou <abouhamad.d@northeastern.edu>
2023-07-10 20:16:28 -0400

    sped up typing again

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -52,7 +52,7 @@ export default function Home() {
         //Iterate through the current line
         return [prev[0], prev[1] + 1]
       });
-      if (loop) setTimeout(typing, getRandomInt(50, 350));
+      if (loop) setTimeout(typing, getRandomInt(20, 250));
     }
     setTimeout(typing, 1000);
   }, [])
